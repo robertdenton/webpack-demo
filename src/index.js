258aae59ee0bd245645ef78cf787c30b0a7fc1a4
Rob Denton <robdentonrg@gmail.com>
2017-10-27 15:03:54 -0700

    before output management

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import img from './img.png';
-import Data from './index.json';
 
 function component(){
     var element = document.createElement('div');
-    var myImg = new Image();
-    myImg.src = img;
 
-    element.innerHTML = _.join(['Hello','webpack','NEW'],' ');
-    element.classList.add('hello');
-    element.appendChild(myImg);
+    element.innerHTML = _.join(['Hello','webpack',],' ');
     
-    console.log(Data);
-
     return element;
 }
 
