5dc6746b917014f575b157ebb95d895b36c59371
Rob Denton <robdentonrg@gmail.com>
2017-10-27 17:31:11 -0700

    Got dev server running

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import printMe from './print.js';
 function component(){
     var element = document.createElement('div');
     var btn = document.createElement('button');
-    element.innerHTML = _.join(['Hello','webpack',],' ');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
+    element.innerHTML = _.join(['foo','baz','bar', 'this is like magic'],' ');
     return element;
 }
 
